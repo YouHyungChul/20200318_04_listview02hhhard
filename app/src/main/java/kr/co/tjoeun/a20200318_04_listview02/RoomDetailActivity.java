d27d48d4c1320df4d496bb05ddecf58bde5dddc4
YouHyungChul <61854958+YouHyungChul@users.noreply.github.com>
2020-03-18 17:21:23 +0900

    방 상세 설정

--- app/src/main/java/kr/co/tjoeun/a20200318_04_listview02/RoomDetailActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200318_04_listview02/RoomDetailActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RoomDetailActivity extends  {
+import kr.co.tjoeun.a20200318_04_listview02.databinding.ActivityRoomDetailBinding;
 
+public class RoomDetailActivity extends BaseActivity {
+    ActivityRoomDetailBinding binding = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_detail);
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
     }
 }
